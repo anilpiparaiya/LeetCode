3a782ff6394aaa1d781c347957d5d710902db51c
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-08-04 22:55:07 +0530

    Two different solution for problem 27. Remove Element

---
+++ 27. Remove Element.cpp
@@ -0,0 +1,43 @@
+// Solution 1
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+         
+        vector<int> :: iterator it;
+        it = nums.begin();
+
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i] == val){
+                nums.erase(it);
+
+                it--;
+                i--;
+            }
+
+            it++;
+        }
+
+        return nums.size();
+        
+    }
+};
+
+
+// Solution 2
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        int index = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+        
+    }
+};
