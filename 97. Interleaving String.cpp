d284032c5f4c769bfb86c2dafd929970972792b7
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-08-25 23:48:34 +0530

    Create 97. Interleaving String.cpp

---
+++ 97. Interleaving String.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool solve(string &s1, string &s2, string &s3,int ind1,int ind2,vector<vector<int>>&dp){
+
+    if(ind1 + ind2 == s3.size()) return 1;
+
+    if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+
+    bool ans=0;   
+
+    if((ind1 < s1.size()) && s1[ind1] == s3[ind1+ind2]) 
+    ans=(ans | solve(s1,s2,s3,ind1+1,ind2,dp));
+    if(ind2<s2.size() && s2[ind2]==s3[ind1+ind2])
+    ans=(ans | solve(s1,s2,s3,ind1,ind2+1,dp));
+    return dp[ind1][ind2]=ans;
+}
+    bool isInterleave(string s1, string s2, string s3) {
+         if(s1.size()+s2.size()!=s3.size()) return false;
+    vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+    return solve(s1,s2,s3,0,0,dp);
+    }
+};
