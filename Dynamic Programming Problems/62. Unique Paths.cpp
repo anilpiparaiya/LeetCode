6b6350b4c42d0af99ee4b210f50b9537e123d130
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-09-03 21:33:46 +0530

    Create 62. Unique Paths.cpp

---
+++ Dynamic Programming Problems/62. Unique Paths.cpp
@@ -0,0 +1,52 @@
+#include <cmath>
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long long ans = 1;
+        for (int i = 1; i <= m - 1; ++i) {
+            ans = ans * (n - 1 + i) / i;
+        }
+        return (int)ans;
+    }
+};
+
+//
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            dp[0][j] = 1;
+        }
+        
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+};
+
+//
+
+
+class Solution {
+ public:
+  int uniquePaths(int m, int n) {
+    // dp[i][j] := unique paths from (0, 0) to (i, j)
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+
+    for (int i = 1; i < m; ++i)
+      for (int j = 1; j < n; ++j)
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+
+    return dp[m - 1][n - 1];
+  }
+};
