162e29297f1b82f9365005ae835d256c740697f9
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-10-05 23:00:14 +0530

    Create 231. Power of Two.cpp

---
+++ Math Problems/231. Power of Two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if(n < 0) return 0;
+
+       while(n != 1){
+           if(n % 2 == 1){
+               return 0;
+           }
+
+           n = n / 2;
+       }
+
+       return 1;
+        
+    }
+};
