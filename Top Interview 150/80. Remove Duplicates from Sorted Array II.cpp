d0d6c9a1e5bb277fa2366902ed669c5a944f46cc
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-10-16 11:19:08 +0530

    Create 80. Remove Duplicates from Sorted Array II.cpp

---
+++ Top Interview 150/80. Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i=0;
+
+        for(int j=0; j<n; j++){
+            if(i<2 || nums[j] > nums[i-2]){
+                nums[i++] = nums[j];
+            }
+        }
+        return i;
+    }
+};
