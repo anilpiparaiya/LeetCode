52d79b38b835580b569e67ca7ef02915cfd6f298
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2023-12-29 23:59:48 +0530

    Create 278._First_Bad_Version.cpp

---
+++ Binary Search Problems/Binary Search Patterns/278._First_Bad_Version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s=1,e=n;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+
+            if(isBadVersion(mid)){
+                e=mid-1;
+            }
+            else if(!isBadVersion(mid)){
+                 s=mid+1;
+            }
+        }
+        return s;
+    }
+};
