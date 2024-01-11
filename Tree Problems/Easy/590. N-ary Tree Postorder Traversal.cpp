049a488204c5b621565806a190e18c76aa41d952
Anil Piparaiya <anilkumarpiparaiya@gmail.com>
2024-01-11 16:16:34 +0530

    N-ary Tree Postorder Traversal

--- Tree Problems/Easy/590. N-ary Tree Postorder Traversal.cpp
+++ Tree Problems/Easy/590. N-ary Tree Postorder Traversal.cpp
@@ -21,22 +21,22 @@ public:
 class Solution {
 public:
     void PostOrder(Node* root, vector<int>&ans){
+        //Base Case if tree is empty
         if(root==NULL) return;
 
+        // PostOrder in all of the children
         for(int i = 0; i < root->children.size(); i++){
             PostOrder(root->children[i], ans);
         }
 
+        // Storing ans
         ans.push_back(root->val);
         return;
     }
+
     vector<int> postorder(Node* root) {
         vector<int>ans;
-
         PostOrder(root, ans);
-
-        return ans;
-
-        
+        return ans; 
     }
 };
