e7d3fc641defbd4ba9be5764aa826ea82510e87a
Anil Piparaiya <78451912+anilpiparaiya@users.noreply.github.com>
2024-01-14 22:54:45 +0530

    Create 113. Path Sum II.cpp

---
+++ Tree Problems/Medium/113. Path Sum II.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void getAllPaths(TreeNode *root,int targetSum,vector<int> temp,vector<vector<int>> &ans){
+        if(root == NULL) return;
+
+        if(root->left == NULL && root->right == NULL && targetSum == root->val){
+            temp.push_back(root->val);
+            ans.push_back(temp);
+            return;
+        }
+
+        temp.push_back(root->val);
+        getAllPaths(root->left,targetSum-root->val,temp,ans);
+        getAllPaths(root->right,targetSum-root->val,temp,ans);
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        getAllPaths(root,targetSum, {}, ans);
+        return ans;
+    }
+};
